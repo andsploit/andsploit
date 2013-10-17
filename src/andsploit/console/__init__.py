f3b6de036bf5405f45d0db5e051cf7b613e5dccd
Tomorrow <liuhm09@gmail.com>
2013-10-18 05:07:40 +0800

    correct console/__init__.py

--- src/andsploit/console/__init__.py
+++ src/andsploit/console/__init__.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-
 __all__ = [ "Console" ]
 
 from andsploit.console.console import Console
-=======
-__all__ = [ "Console" ]
-
-from andsploit.console.console import Console
->>>>>>> 7289a61d63e8580db24c62f2697b14beb495a945
